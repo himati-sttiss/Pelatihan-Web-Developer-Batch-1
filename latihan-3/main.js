ae5032ae403ae716751bc1918d3b2bdafc19293e
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-12-01 22:25:33 +0700

    Upload Materi Latihan 3

---
+++ latihan-3/main.js
@@ -0,0 +1,25 @@
+// Variabel
+
+// var,  let, const
+
+// TIPE DATA :
+
+// 1. string
+// 2. integer
+
+var a1 = 1;
+var a2 = a1;
+
+let a = 3;
+let b = 2;
+let c = 1;
+let d;
+
+d = a; // 3
+a = c; // 1
+c = d; // 3
+
+
+console.log('a =', a);
+console.log('b =', b);
+console.log('c =', c);
